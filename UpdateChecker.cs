28e9705fb422c7516343e38f80431bdb658b5415
J4gg3d <nightman-e@gmx.net>
2026-01-27 16:16:19 +0100

    Fix missing native SimConnect.dll in release
    - Add <Link> element to copy SimConnect.dll to output root instead of libs/ subfolder
    - Bump version to 1.8.1

--- UpdateChecker.cs
+++ UpdateChecker.cs
@@ -7,7 +7,7 @@ namespace MSFSBridge;
 
 public class UpdateChecker
 {
-    public const string CURRENT_VERSION = "1.8.0";
+    public const string CURRENT_VERSION = "1.8.1";
     private const string GITHUB_REPO = "J4gg3d/simchecklist-bridge";
     private const string GITHUB_API = $"https://api.github.com/repos/{GITHUB_REPO}/releases/latest";
 
